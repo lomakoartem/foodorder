78b6f0ba38b773f528e3e0fe23717d7bfa13fd56
Anton Babak <Anton_Babak@epam.com>
2016-05-30 17:37:14 +0300

    change entity IDs type to bigint and add version

--- scriptsDB/script_createDB.sql
+++ scriptsDB/script_createDB.sql
@@ -3,16 +3,18 @@ CREATE SEQUENCE auto_id_user;
 CREATE SEQUENCE auto_id_role;
 
 CREATE TABLE locations(
-	location_id integer DEFAULT nextval('auto_id_location') PRIMARY KEY,
+	location_id bigint DEFAULT nextval('auto_id_location') PRIMARY KEY,
 	location_name varchar(5) NOT NULL,
 	location_address varchar(25) NOT NULL,
 	location_info varchar(50),
 	location_isActive boolean NOT NULL DEFAULT TRUE
+	version integer 
 );
 
 CREATE TABLE user_roles(
-	role_id integer DEFAULT nextval('auto_id_role') PRIMARY KEY,
+	role_id bigint DEFAULT nextval('auto_id_role') PRIMARY KEY,
 	role_name varchar(25) NOT NULL
+	version integer
 );
 
 CREATE TABLE users(
@@ -21,6 +23,7 @@ CREATE TABLE users(
 	user_password varchar(150) NOT NULL,
 	user_name varchar(50) NOT NULL,
 	user_phone varchar(13) NOT NULL,
-	user_role integer NOT NULL REFERENCES user_roles(role_id) ON DELETE RESTRICT ON UPDATE CASCADE
+	user_role bigint NOT NULL REFERENCES user_roles(role_id) ON DELETE RESTRICT ON UPDATE CASCADE
+	version integer
 );
 
