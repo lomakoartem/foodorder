ac11b8dae098b042b8334842e86f003c6ee54e16
Anton Babak <Anton_Babak@epam.com>
2016-05-30 17:44:32 +0300

    add commas

--- scriptsDB/script_createDB.sql
+++ scriptsDB/script_createDB.sql
@@ -7,13 +7,13 @@ CREATE TABLE locations(
 	location_name varchar(5) NOT NULL,
 	location_address varchar(25) NOT NULL,
 	location_info varchar(50),
-	location_isActive boolean NOT NULL DEFAULT TRUE
+	location_isActive boolean NOT NULL DEFAULT TRUE,
 	version integer 
 );
 
 CREATE TABLE user_roles(
 	role_id bigint DEFAULT nextval('auto_id_role') PRIMARY KEY,
-	role_name varchar(25) NOT NULL
+	role_name varchar(25) NOT NULL,
 	version integer
 );
 
@@ -23,7 +23,7 @@ CREATE TABLE users(
 	user_password varchar(150) NOT NULL,
 	user_name varchar(50) NOT NULL,
 	user_phone varchar(13) NOT NULL,
-	user_role bigint NOT NULL REFERENCES user_roles(role_id) ON DELETE RESTRICT ON UPDATE CASCADE
+	user_role bigint NOT NULL REFERENCES user_roles(role_id) ON DELETE RESTRICT ON UPDATE CASCADE,
 	version integer
 );
 
